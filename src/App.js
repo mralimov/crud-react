a74fb551f4b26c0d3df86642786e0b4893c6802f
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-10-14 11:11:49 -0400

    Routing components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import AddUser from './components/AddUser';
+import EditUser from './components/EditUser';
+import Home from './components/Home';
+import { Router, Switch, Link, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello</h1>
+    <div style={{ maxWidth: '30rem', margin: '0 auto' }}>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route exact path='/addUser'>
+        <AddUser />
+      </Route>
+      <Route exact path='/editUser:id'>
+        <EditUser />
+      </Route>
+      {/* <Route path='*'>
+        <h1>Page Not Fount</h1>
+      </Route> */}
     </div>
   );
 }
--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
 const AddUser = () => {
   return (
     <div>
-      <h1>Add User</h1>
+      <Form>
+        <FormGroup>
+          <Label htmlFor='userInput'>Name</Label>
+          <Input type='text' placeholder='Enter Name' id='userInput'></Input>
+        </FormGroup>
+        <Button type='submit'>Submit</Button>
+      </Form>
     </div>
   );
 };
